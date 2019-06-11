b4ddbe76fb37f7ba06c23af573d499ce9c4ec95c
njerucyrus <njerucyrus5@gmail.com>
2019-06-11 14:14:44 +0300

    updated new-offer module

--- src/app/places/offers/new-offer/new-offer.module.ts
+++ src/app/places/offers/new-offer/new-offer.module.ts
@@ -17,7 +17,6 @@ const routes: Routes = [
 @NgModule({
     imports: [
         CommonModule,
-        FormsModule,
         IonicModule,
         ReactiveFormsModule,
         RouterModule.forChild(routes),
